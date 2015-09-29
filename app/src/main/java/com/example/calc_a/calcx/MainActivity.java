dd0561f5841af67a328f07024b7100bce0517bd8
ankush <ankush.patial86@gmail.com>
2015-09-29 23:16:53 +0530

    1.0

--- app/src/main/java/com/example/calc_a/calcx/MainActivity.java
+++ app/src/main/java/com/example/calc_a/calcx/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calc_a.calcx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,10 +17,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String Tag = "message";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     EditText e_bill;
     RadioGroup rgb_RsType;
-    RadioButton rb_ac, rb_nac;
     Button   b_result;
     TextView t_st, t_sc, t_vat;
 
@@ -30,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         e_bill = (EditText)findViewById(R.id.AmountInput);
 
-        rb_ac =(RadioButton)findViewById(R.id.AC);
-        //rb_nac=(RadioButton)findViewById(R.id.NonAC);
+        rgb_RsType = (RadioGroup)findViewById(R.id.Rtype);
 
         b_result = (Button)findViewById(R.id.button3);
 
@@ -39,32 +38,49 @@ public class MainActivity extends AppCompatActivity {
         t_sc = (TextView)findViewById(R.id.SC2);
         t_vat = (TextView)findViewById(R.id.vt2);
 
-
-         b_result.setOnClickListener(new View.OnClickListener() {
+        b_result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                     try {
+                try {
+
+                    double n1 = 0;
+                    double n2 = 0;
+                    double n3 = 0;
+                    double n4 = 0;
+                    double temp = 0;
+                    // get selected radio button from radioGroup
+                    int selectedId = rgb_RsType.getCheckedRadioButtonId();
+
+                    n1 = Double.parseDouble(e_bill.getText().toString());
+
+                    switch(selectedId)
+                    {
+                        case R.id.AC:
+                            Log.v(LOG_TAG, "AC button clicked");
+                            temp = n1 / 1.31384;
+                            n2 = temp / 10;
+                            n4 = (temp + n2) * 0.145;
+                            n3 = ((temp + n2) * 0.4) * 0.1236;
+                            break;
+                        case R.id.NonAC:
+                            Log.v(LOG_TAG, "NonAC button clicked");
+
+                            n3 = 0;
+                          // Ninjas rule
+                            break;
+                    }
 
 
-                        double n1 = 0;
-                        double n2 = 0;
-                        double n3 = 0;
-                        double n4 = 0;
-                        double temp = 0;
-                        if (TextUtils.isEmpty(e_bill.getText().toString())) {
-                            return;
-                        }
+                    if (TextUtils.isEmpty(e_bill.getText().toString())) {
+                        return;
+                    }
 
-                        n1 = Double.parseDouble(e_bill.getText().toString());
-                        n2 = n1 / 10;
-                        n3 = ((n1 + n2) * 0.4) * 0.1236;
-                        n4 = temp * 0.1236;
-                        n4 = (n1 + n2) * 0.145;
 
-                        t_sc.setText(" " + n2);
-                        t_st.setText(" " + n3);
-                        t_vat.setText(" " + n4);
+
+                    t_sc.setText(String.format(" %.2f", n2));
+                    t_st.setText(String.format(" %.2f", n3));
+                    t_vat.setText(String.format(" %.2f", n4));
 
 
                 }  catch (Exception e) {
@@ -74,6 +90,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.AC:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.NonAC:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 
     @Override
